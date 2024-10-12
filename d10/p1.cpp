3b897db8f82c469564610c242684a70578831a31
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2024-10-12 23:34:49 +0300

    d10

---
+++ d10/p1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+std::string look_and_say(std::string in) {
+	std::string ans;
+	char prev_letter = in[0];
+	int prev_times = 1;
+
+	for (int i = 1; i < in.size(); i++) {
+		if (prev_letter == in[i]) {
+			prev_times++;
+		} else {
+			ans += std::to_string(prev_times);
+			ans += prev_letter;
+
+			prev_letter = in[i];
+			prev_times = 1;
+		}
+	}
+	ans += std::to_string(prev_times);
+	ans += prev_letter;
+
+	return ans;
+}
+
+int main() {
+	std::string input;
+	std::getline(std::cin, input);
+
+	for (int i = 0; i < 40; i++) {
+		input = look_and_say(input);
+	}
+
+	std::cout << input.size() << '\n';
+	return 0;
+}
