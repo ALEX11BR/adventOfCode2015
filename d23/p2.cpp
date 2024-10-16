a35fa3531b93a503d3c61f843e489a56c94285ec
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2024-10-16 20:28:22 +0300

    d23

---
+++ d23/p2.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <array>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <random>
+#include <regex>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <streambuf>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+int main() {
+	std::string input;
+
+	std::vector<std::string> instructions;
+	while (std::getline(std::cin, input)) {
+		instructions.push_back(input);
+	}
+
+	int i = 0;
+	std::map<std::string, long long> reg;
+	reg["a"] = 1;
+	reg["b"] = 0;
+
+	while (i < instructions.size()) {
+		if (instructions[i].substr(0, 3) == "hlf") {
+			reg[instructions[i].substr(4)] /= 2;
+			i++;
+		} else if (instructions[i].substr(0, 3) == "tpl") {
+			reg[instructions[i].substr(4)] *= 3;
+			i++;
+		} else if (instructions[i].substr(0, 3) == "inc") {
+			reg[instructions[i].substr(4)]++;
+			i++;
+		} else if (instructions[i].substr(0, 3) == "jmp") {
+			i += std::stoi(instructions[i].substr(4));
+		} else if (instructions[i].substr(0, 3) == "jie") {
+			if (reg[instructions[i].substr(4, 1)] % 2 == 0) {
+				i += std::stoi(instructions[i].substr(7));
+			} else {
+				i++;
+			}
+		} else if (instructions[i].substr(0, 3) == "jio") {
+			if (reg[instructions[i].substr(4, 1)] == 1) {
+				i += std::stoi(instructions[i].substr(7));
+			} else {
+				i++;
+			}
+		}
+	}
+
+	std::cout << reg["b"] << '\n';
+	return 0;
+}
