b1d57b07e3ca7ae9fc0fd1f503281df3a0253cdf
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2024-10-16 19:14:04 +0300

    Don't use bits stdc++

--- d11/p1.cpp
+++ d11/p1.cpp
@@ -1,4 +1,23 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <random>
+#include <regex>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <streambuf>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
 std::string increment(std::string in) {
 	int i = in.size() - 1;
